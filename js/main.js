636f6f8aebf65c3bce6a8ae334090f18b74f02c7
Marwil Campos <marwilcampos@gmail.com>
2018-11-13 20:33:12 -0400

    css

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function teclear(tecla){
         case  '8':
         case  '9':
         case  '/':
-        case  'X':
+        case  '*':
         case  '-':
         case  '+':
             $('#display').val(input + tecla);
@@ -30,11 +30,25 @@ function teclear(tecla){
 
 function operar(input){
     let result = 0;
-
+    try {
+        result = eval(input);
+    }catch (e) {
+        result = 'Expression invalida';
+    }
     return result;
 }
 
 $(document).ready(function ( ) {
+    /* Esta funcion cambia del color del
+    * */
+    $('#selector').change(function(){
+        let select = $(this).children('option:selected').val();
+        console.log(select);
+        $('.grid-container').removeClass('azul');
+        $('.grid-container').removeClass('gris');
+        $('.grid-container').removeClass('rojo');
+        $('.grid-container').addClass(select);
+    });
 
 
 /*
